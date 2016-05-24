58292215af1370c2dfd93565b15695eb936b2eae
Ricardo Luiz <ric_luiz@outlook.com>
2016-05-24 18:22:59 -0300

    Commit inicial do projeto. Ja esta com o tanque,mapa e 3 scripts para controlar a camera,canhao do tanque e o proprio tanque.

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public int potencia;
+	public int speed;
+	public int rotacao;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		speed *= potencia;
+	}
+
+	void Update(){
+	}
+
+	void FixedUpdate(){
+		if(Input.GetKey(KeyCode.W)){
+			rb.AddForce(transform.forward*speed*Time.deltaTime);
+		} else if(Input.GetKey(KeyCode.S)){
+			rb.AddForce(-transform.forward*speed*Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.up*-rotacao*Time.deltaTime);
+		} else if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(Vector3.up*rotacao*Time.deltaTime);
+		}
+	}
+}
