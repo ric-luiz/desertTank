22ce25cd64e4356e3e193b6707880913dc6c570d
Ricardo Luiz <ric_luiz@outlook.com>
2016-07-18 20:45:25 -0300

    Adicionado prefab do inimigo com as configurações já ajustadas.

---
+++ Assets/scripts/enemy/EnemyCanhaoController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCanhaoController : Canhao {
+	private GameObject target;
+
+	public override void Start ()
+	{
+		angularSpeed = 20.0f;
+		target = GameObject.FindGameObjectWithTag ("Player");
+		base.Start ();
+	}
+
+	protected override void FixedUpdate ()
+	{		
+		position = target.transform.position - transform.position;
+		setarRotacaoBase (position);
+		setarRotacaoCanhao (position);
+	}
+
+	protected override void setarDirecaoRotacao ()
+	{		
+		direcaoCorpo = corpo.GetComponent<EnemyController> ().getDirecaoRotacao ();
+	}
+}
